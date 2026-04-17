ff4d250f09341c480ec11914fe88f6b32de53678
LeeVuKhang <lvukhangpq@gmail.com>
2026-04-17 07:34:30 +0700

    Remove unnecessary button

--- client/src/components/header/TopBars.js
+++ client/src/components/header/TopBars.js
@@ -82,11 +82,11 @@ function TopBars({handleSeachProduct}) {
             <span className="top-bar-right-badge">{cartCount}</span>
           )}
         </Link>
-        <Link to='/stores' className="top-bar-right-action">
+        {/* <Link to='/stores' className="top-bar-right-action">
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z"/><circle cx="12" cy="10" r="3"/>
           </svg>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
